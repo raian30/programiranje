b5bb5985f2c8a746afdf6879d8216c187529c55d
Raian <96938727+raian30@users.noreply.github.com>
2025-10-01 09:32:35 +0200

    Second commit

--- Raian Melon/main.py
+++ Raian Melon/main.py
@@ -1,35 +1,42 @@
-#Funkcija za učitavanje teksta iz datoteke
+#Funkcija za učitavanje teksta iz datoteke 
 def ucitaj_tekst(filepath):
     try:
-        # ovdje ide logika za citanje datoteke
-        with open(filepath, 'r', encoding='utf-8') as file:
+        # KOd za otvaranje datoteke ide ovdje
+        with open (filepath, 'r', encoding='utf-8') as file:
             sadrzaj = file.read()
         return sadrzaj
     except FileNotFoundError:
-        print(f"Datoteka {filepath} nije pronađena.")
-        return None
-
-#Funkcija za prociscavanje teksta
-def procisti_tekst(tekst):
-    # ovdje ide logika za prociscavanje teksta
+        print(f"Greška: Datoteka na putanji '{filepath}' nije pronađena")
+        return None # Vratit ćemo 'ništa' ako datoteka ne postoji
+      
+      # Funkcija za proišćavanje teksta 
+def ocisti_tekst(tekst):
+#Kod za pročišćavanje teksta ide ovdje
     tekst = tekst.lower()
-    return tekst
 
-if __name__ == "__main__":
-    filepath = 'tekst.txt'
-    print(f"Pokušavam učitati datoteku: {filepath}")
-    tekst = ucitaj_tekst(filepath)
-    if tekst:
-        print("Sadržaj datoteke:")
-        print(tekst)
-    else:
-        print("Nije moguće učitati sadržaj datoteke.")
-    
-    tekst_prociscen = procisti_tekst(tekst)
-    if tekst_prociscen:
-        print("Pročišćeni tekst:")
-        print(tekst_prociscen)
-    else:
-        print("Nije moguće pročitati ili pročišćiti tekst.")
+    interpunkcija = ['.', ',', '!', '?', ':', '"', "'", '(', ')']
+    for znak in interpunkcija:
+        tekst = tekst.replace(znak, '')
 
-    
+    lista_rijeci = tekst.split()
+
+    return lista_rijeci
+       
+if __name__=="__main__":
+    filepath = "tekst.txt"
+    print(f"Učitavam tekst iz datoteke: {filepath}")
+    ucitani_tekst = ucitaj_tekst(filepath)
+    if ucitani_tekst:
+        print("Učitani tekst je:")
+        #print(ucitani_tekst)
+    else:
+        print("Greška pri učitavanju datoteke.")
+
+    ucitani_tekst = ocisti_tekst(ucitani_tekst)
+
+    if ucitani_tekst:
+        print("Očišćeni tekst je:")
+        print(ucitani_tekst)
+    else:
+        print("Greška pri očišćavanju teksta.")
+        
